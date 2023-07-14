e25ae8715e13062507d41bcd0b15f677ac55c1f3
Sangheeoh <99467752+hiosfuture@users.noreply.github.com>
2023-07-14 11:26:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,95 @@
-// 스크립트 내용
-document.getElementById('login-button').addEventListener('click', function() {
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
+// script.js
 
-  // 로그인 동작 등 추가 구현
-  console.log('Username:', username);
-  console.log('Password:', password);
+document.addEventListener('DOMContentLoaded', function() {
+  const passwordForm = document.getElementById('password-form');
+  const passwordList = document.getElementById('password-list');
+
+  // 데이터 전송 및 저장
+  passwordForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const title = document.getElementById('title').value;
+    const url = document.getElementById('url').value;
+    const id = document.getElementById('id').value;
+    const password = document.getElementById('password').value;
+    const user = document.getElementById('user').value;
+    const updatedAt = new Date().toISOString();
+
+    saveData(title, url, id, password, user, updatedAt);
+
+    passwordForm.reset();
+  });
+
+  // 저장된 데이터 조회
+  getAllData().then(function(data) {
+    renderDataList(data);
+  });
+
+  // 저장된 데이터 리스트 표시
+  function renderDataList(data) {
+    passwordList.innerHTML = '';
+
+    data.forEach(function(item) {
+      const li = document.createElement('li');
+      li.innerHTML = `
+        <div>
+          <strong>Title:</strong> ${item.title}<br>
+          <strong>URL:</strong> ${item.url}<br>
+          <strong>ID:</strong> ${item.id}<br>
+          <strong>Password:</strong> ${item.password}<br>
+          <strong>User:</strong> ${item.user}<br>
+          <strong>Updated At:</strong> ${item.updated_at}<br>
+        </div>
+        <button class="edit-button" data-id="${item.id}">Edit</button>
+        <button class="delete-button" data-id="${item.id}">Delete</button>
+      `;
+
+      passwordList.appendChild(li);
+    });
+  }
+
+  // 데이터 저장
+  async function saveData(title, url, id, password, user, updatedAt) {
+    const response = await fetch('/save-data', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: title,
+        url: url,
+        id: id,
+        password: password,
+        user: user,
+        updatedAt: updatedAt
+      })
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      renderDataList(data);
+    } else {
+      console.error('Failed to save data.');
+    }
+  }
+
+  // 데이터 삭제
+  passwordList.addEventListener('click', function(event) {
+    if (event.target.classList.contains('delete-button')) {
+      const id = event.target.dataset.id;
+      deleteData(id);
+    }
+  });
+
+  async function deleteData(id) {
+    const response = await fetch(`/delete-data/${id}`, {
+      method: 'DELETE'
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      renderDataList(data);
+    } else {
+      console.error('Failed to delete data.');
+    }
+  }
 });
