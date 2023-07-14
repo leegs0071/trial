afc9a1e643880009f0dd9a51a7a8c1e62a654112
Sangheeoh <99467752+hiosfuture@users.noreply.github.com>
2023-07-14 13:25:49 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const { Pool } = require('pg');
 const connectionString = 'ep-snowy-king-089485-pooler.ap-southeast-1.postgres.vercel-storage.com';
+
 const pool = new Pool({
   connectionString,
   ssl: {
