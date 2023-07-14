715e34b5a2d098381c6acc0afd9f9f000edcb753
Sangheeoh <99467752+hiosfuture@users.noreply.github.com>
2023-07-14 11:40:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// script.js
+//script.js
 
 document.addEventListener('DOMContentLoaded', function() {
   const passwordForm = document.getElementById('password-form');
@@ -14,82 +14,60 @@ document.addEventListener('DOMContentLoaded', function() {
     const user = document.getElementById('user').value;
     const updatedAt = new Date().toISOString();
 
-    saveData(title, url, id, password, user, updatedAt);
+    savePassword(title, url, id, password, user, updatedAt);
 
     passwordForm.reset();
   });
 
   // 저장된 데이터 조회
-  getAllData().then(function(data) {
-    renderDataList(data);
-  });
+  getPasswords();
 
-  // 저장된 데이터 리스트 표시
-  function renderDataList(data) {
-    passwordList.innerHTML = '';
+  // 비밀번호 저장
+  function savePassword(title, url, id, password, user, updatedAt) {
+    const data = {
+      title: title,
+      url: url,
+      id: id,
+      password: password,
+      user: user,
+      updatedAt: updatedAt
+    };
 
-    data.forEach(function(item) {
-      const li = document.createElement('li');
-      li.innerHTML = `
-        <div>
-          <strong>Title:</strong> ${item.title}<br>
-          <strong>URL:</strong> ${item.url}<br>
-          <strong>ID:</strong> ${item.id}<br>
-          <strong>Password:</strong> ${item.password}<br>
-          <strong>User:</strong> ${item.user}<br>
-          <strong>Updated At:</strong> ${item.updated_at}<br>
-        </div>
-        <button class="edit-button" data-id="${item.id}">Edit</button>
-        <button class="delete-button" data-id="${item.id}">Delete</button>
-      `;
-
-      passwordList.appendChild(li);
-    });
-  }
-
-  // 데이터 저장
-  async function saveData(title, url, id, password, user, updatedAt) {
-    const response = await fetch('/save-data', {
+    fetch('/api/passwords', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        title: title,
-        url: url,
-        id: id,
-        password: password,
-        user: user,
-        updatedAt: updatedAt
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      getPasswords();
+    })
+    .catch(error => {
+      console.error('Failed to save password:', error);
+    });
+  }
+
+  // 저장된 데이터 조회
+  function getPasswords() {
+    fetch('/api/passwords')
+      .then(response => response.json())
+      .then(data => {
+        renderPasswordList(data);
       })
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      renderDataList(data);
-    } else {
-      console.error('Failed to save data.');
-    }
+      .catch(error => {
+        console.error('Failed to get passwords:', error);
+      });
   }
 
-  // 데이터 삭제
-  passwordList.addEventListener('click', function(event) {
-    if (event.target.classList.contains('delete-button')) {
-      const id = event.target.dataset.id;
-      deleteData(id);
-    }
-  });
+  // 저장된 데이터 리스트 표시
+  function renderPasswordList(passwords) {
+    passwordList.innerHTML = '';
 
-  async function deleteData(id) {
-    const response = await fetch(`/delete-data/${id}`, {
-      method: 'DELETE'
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      renderDataList(data);
-    } else {
-      console.error('Failed to delete data.');
-    }
-  }
-});
+    passwords.forEach(password => {
+      const li = document.createElement('li');
+      li.innerHTML = `
+        <div>
+          <strong>Title:</strong> ${password.title}<br>
+          <strong>URL:</strong> ${
