1bf45a6806f20bc8317b2346ebef0ac66cd91bd2
Sangheeoh <99467752+hiosfuture@users.noreply.github.com>
2023-07-14 11:18:57 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// 스크립트 내용
+document.getElementById('login-button').addEventListener('click', function() {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  // 로그인 동작 등 추가 구현
+  console.log('Username:', username);
+  console.log('Password:', password);
+});
