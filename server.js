706ee8c875f96d17014a82edfa31a2fa01845345
Sangheeoh <99467752+hiosfuture@users.noreply.github.com>
2023-07-14 15:10:42 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,51 @@
+// server.js
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+const express = require('express');
+const { Pool } = require('pg');
+const connectionString = 'ep-snowy-king-089485-pooler.ap-southeast-1.postgres.vercel-storage.com';
+
+// Create Express app
+const app = express();
+
+// Connect to PostgreSQL pool
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+// Define your API routes as Serverless Functions
+app.post('/api/passwords', async (req, res) => {
+  // Handle your API endpoint logic here
+});
+
+app.get('/api/passwords', async (req, res) => {
+  // Handle your API endpoint logic here
+});
+
+// Handle other requests with Next.js
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
+nextApp.prepare().then(() => {
+  // Create HTTP server
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname } = parsedUrl;
+
+    // Handle API requests with Express app
+    if (pathname.startsWith('/api')) {
+      app(req, res);
+    } else {
+      // Handle other requests with Next.js
+      handle(req, res, parsedUrl);
+    }
+  }).listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
