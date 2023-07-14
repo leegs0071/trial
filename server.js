3f7c5663b0b5dd7b15daa0035ca83447db6e2254
Sangheeoh <99467752+hiosfuture@users.noreply.github.com>
2023-07-14 11:31:41 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+// 데이터 저장
+async function saveData(title, url, id, password, user, updatedAt) {
+  const response = await fetch('/save-data', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title: title,
+      url: url,
+      id: id,
+      password: password,
+      user: user,
+      updatedAt: updatedAt
+    })
+  });
+
+  if (response.ok) {
+    getAllData(); // 데이터 저장 후 데이터 다시 조회
+  } else {
+    console.error('Failed to save data.');
+  }
+}
+
+// 저장된 데이터 조회
+async function getAllData() {
+  const response = await fetch('/get-all-data');
+  if (response.ok) {
+    const data = await response.json();
+    renderDataList(data);
+  } else {
+    console.error('Failed to get data.');
+  }
+}
+
