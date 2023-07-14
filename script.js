25b43b1616272d88d3f78dfe90cd3dcc38e0e0a0
Sangheeoh <99467752+hiosfuture@users.noreply.github.com>
2023-07-14 13:43:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,10 @@ document.addEventListener('DOMContentLoaded', function() {
     event.preventDefault();
     const title = document.getElementById('title').value;
     const url = document.getElementById('url').value;
-const id = document.getElementById('id').value;
-const password = document.getElementById('password').value;
+    const id = document.getElementById('id').value;
+    const password = document.getElementById('password').value;
 
-savePassword(title, url, id, password);
-
+    savePassword(title, url, id, password);
 
     passwordForm.reset();
   });
@@ -22,30 +21,29 @@ savePassword(title, url, id, password);
   getPasswords();
 
   // 비밀번호 저장
-function savePassword(title, url, id, password) {
-  const data = {
-    title: title,
-    url: url,
-    id: id,
-    password: password
-  };
+  function savePassword(title, url, id, password) {
+    const data = {
+      title: title,
+      url: url,
+      id: id,
+      password: password
+    };
 
-  fetch('/api/passwords', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-  .then(response => response.json())
-  .then(data => {
-    getPasswords();
-  })
-  .catch(error => {
-    console.error('Failed to save password:', error);
-  });
-}
-
+    fetch('/api/passwords', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(data => {
+        getPasswords();
+      })
+      .catch(error => {
+        console.error('Failed to save password:', error);
+      });
+  }
 
   // 저장된 데이터 조회
   function getPasswords() {
@@ -69,8 +67,8 @@ function savePassword(title, url, id, password) {
         <div>
           <strong>Title:</strong> ${password.title}<br>
           <strong>URL:</strong> ${password.url}<br>
-          <strong>ID Info:</strong> ${password.idinfo}<br>
-          <strong>Password Info:</strong> ${password.pwinfo}
+          <strong>ID Info:</strong> ${password.id}<br>
+          <strong>Password Info:</strong> ${password.password}
         </div>
         <div>
           <button class="edit-button" data-id="${password.id}">Edit</button>
